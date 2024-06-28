1c17aacfb4e66ab8af0568fb6e26bffda4d27a0f
Imon <contact@imonweb.co.uk>
2024-06-28 23:53:59 +0100

    canvas model

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,7 +10,8 @@ import CameraRig from './CameraRig';
 const CanvasModel = () => {
   return (
     <Canvas>
-      
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
     </Canvas>
   )
 }
