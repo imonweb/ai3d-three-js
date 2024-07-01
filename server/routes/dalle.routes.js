68a4db7be436fa871f3af247d21bd25a5d614471
Imon <contact@imonweb.co.uk>
2024-07-01 20:06:40 +0100

    ai picker

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-// import OpenAI from 'openai';
-import { Configuration, OpenAIAPI } from 'openai';
+// import { Configuration, OpenAIApi} from 'openai';
+import { OpenAI } from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
+// const config = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(config);
+// const openai = new OpenAIApi(config);
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -22,7 +22,7 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.images.generate({
+    const response = await openai.createImage({
       prompt,
       n: 1,
       size: '1024x1024',
